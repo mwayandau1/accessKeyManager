90a1608c5fd61e18c1a037f2ffb7f29473e86611
mwayandau1 <global-mosesayandau1@gmail.com>
2024-05-06 12:05:31 +0000

    Added login link to verifySuccess view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import Home from "./pages/Home";
 import SingleKey from "./pages/SingleKey";
-// import VerifyEmail from "./pages/VerifyEmail";
 import SearchKey from "./pages/SearchKey";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -20,7 +19,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          {/* <Route path="/verify-email/:token" element={<VerifyEmail />} /> */}
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
           <Route path="/email-sent/:email" element={<EmailSentPage />} />
